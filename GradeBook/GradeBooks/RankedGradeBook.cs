3c2dc78d99e8a290ecfc0790d0d2d56524597ed6
Sooloken <plaato93@gmail.com>
2018-04-09 15:04:30 +0200

    Soloken
    Test

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,11 +14,19 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if(Students.Count < 5)
+            if (Students.Count < 5)
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-
+            else if (averageGrade >0.80 )
+                return 'A';
+            else if (averageGrade > 0.60 && averageGrade < 0.80)
+                return 'B';
+            else if (averageGrade > 0.40 && averageGrade < 0.60)
+                return 'C';
+            else if (averageGrade > 0.20 && averageGrade < 0.40 )
+                return 'D';
+         
             return 'F';
         }
     }
