e97f9cc3b1e8b65287319fc1730add97bc9b74cf
Sooloken <plaato93@gmail.com>
2018-04-09 16:13:37 +0200

    We
    weeeee

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -39,7 +39,9 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked-grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
             }
+            base.CalculateStatistics();
         }
 
         public override void CalculateStudentStatistics(string name)
